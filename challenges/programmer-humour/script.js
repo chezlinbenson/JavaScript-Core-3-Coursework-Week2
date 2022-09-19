c21e82cae913090462c6364a7c78bedf4c9bc2eb
chezlinbenson <chezlinbenson11@gmail.com>
2022-09-20 01:59:09 +0200

    Completed basic HTML, CSS style sheet, Javascript Fetch API data display in DOM and error handling.

---
+++ challenges/programmer-humour/script.js
@@ -0,0 +1,31 @@
+function getFunnyImage (){
+    fetch("https://xkcd.now.sh/?comic=latest")
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          throw new Error(
+            `Encountered something unexpected: ${response.status} ${response.statusText}`
+          );
+        }
+      })
+      .then((jsonResponse) => {
+        // do whatever you want with the JSON response
+        let body = document.querySelector("body");
+        let imageEl = document.createElement("img");
+        console.log(jsonResponse);
+        imageEl.src = jsonResponse.img;
+        body.appendChild(imageEl);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.log(error);
+      });
+}
+
+
+
+
+
+
+
